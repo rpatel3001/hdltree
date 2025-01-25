6614743ef993518646fc4d95cca0561d2b952945
Rajan Patel <rpatel3001@gmail.com>
2025-01-25 17:18:35 -0500

    remove incomplete HDL files

--- docs/images/logo.vhdl
+++
@@ -1,6 +0,0 @@
-component logo is
-  port (
-    a,b,c : in bit_vector(1 downto 0);
-    x,y : out bit_vector(1 downto 0)
-  );
-end component;
--- docs/images/scaled.vhdl
+++
@@ -1,8 +0,0 @@
-component scaled is
-  port (
-    Clock : in  std_ulogic;
-    Reset : in  std_ulogic;
-    Din   : in  unsigned;
-    Dout  : out unsigned
-  );
-end component;
