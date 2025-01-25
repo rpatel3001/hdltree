2ba309a7df0ff7093a557bdc0e532837d1b508b4
Rajan Patel <rpatel3001@gmail.com>
2025-01-25 18:19:33 -0500

    add new main() that parses files passed as args. add ambig option to parsers

--- hdltree/hdltree.py
+++ hdltree/hdltree.py
@@ -4,8 +4,12 @@ from pathlib import Path
 from json import dumps, loads
 from lark import Lark, logger, ast_utils
 from lark_ambig_tools import CountTrees
-from lark.exceptions import VisitError
+from lark.exceptions import UnexpectedCharacters
 from rich import print as richprint
+from argparse import ArgumentParser
+from io import StringIO, TextIOBase
+from time import time
+from typing import List
 
 from . import VhdlParseTreeTransformers
 from . import VhdlCstTransformer
@@ -50,32 +54,41 @@ def fix_pypy_console():
                             f.reconfigure(encoding=cur_encoding)
 
 
+vhdl_fileext = ["vhd", "vhdl", "vho", "vht"]
+vlog_fileext = ["v", "vh", "verilog", "vlg", "vo", "vqm", "vt", "veo", "sv", "svh", "vlog"]
+
+
 def filetype(fpath: Path):
     fileext = fpath.suffix[1:]
-    if fileext in ["vhd", "vhdl", "vho", "vht"]:
+    if fileext in vhdl_fileext:
         return "VHDL"
-    elif fileext in ["v", "vh", "verilog", "vlg", "vo", "vqm", "vt", "veo", "sv", "svh", "vlog"]:
+    elif fileext in vlog_fileext:
         return "VLOG"
     else:
         return fileext.upper()
 
 
 class HdlParser:
-    def __init__(self, use_regex=True, debug=False):
+    def __init__(self, ambig=False, use_regex=True, debug=False):
         if debug:
             logger.setLevel(logging.DEBUG)
-        self.vhdl_parser = VhdlParser(use_regex, debug)
-        self.vlog_parser = VerilogParser(use_regex, debug)
+        self.vhdl_parser = VhdlParser(ambig, use_regex, debug)
+        self.vlog_parser = VerilogParser(ambig, use_regex, debug)
 
-    def parseFile(self, fpath: Path | str, ftype: str = ""):
+    def parseFile(self, fpath: TextIOBase | Path | str, ftype: str = ""):
         if isinstance(fpath, str):
             fpath = Path(fpath)
+
         ftype = ftype.upper()
         if ftype not in ["VHDL", "VLOG"]:
             ftype = filetype(fpath)
 
-        txt = fpath.read_text("latin-1")
-        return self.parse(txt, ftype)
+        if isinstance(fpath, Path):
+            txt = fpath.read_text("latin-1")
+            return self.parse(txt, ftype)
+        elif isinstance(fpath, TextIOBase):
+            txt = fpath.read()
+            return self.parse(txt, ftype)
 
     def parse(self, txt: str, ftype: str = "VHDL"):
         if ftype == "VHDL":
@@ -87,15 +100,38 @@ class HdlParser:
 
 
 class VerilogParser:
-    def __init__(self, use_regex=True, debug=False):
-        pass
+    def __init__(self, ambig=False, use_regex=True, debug=False):
+        try:
+            import regex
 
-    def parseFile(self, fpath: Path | str, ftype: str = ""):
+            use_regex = True
+        except ModuleNotFoundError:
+            use_regex = False
+
+        self.ambig = ambig
+
+        # self.parser = Lark(
+        #    open(Path(__file__).parent / "verilog.lark", encoding="latin-1"),
+        #    start="design_file",
+        #    regex=use_regex,
+        #    debug=debug,
+        #    ambiguity="explicit" if ambig else "resolve",
+        #    lexer="dynamic",
+        # )
+        # self.csttransformer = ast_utils.create_transformer(
+        #    VerilogCstTransformer, VerilogParseTreeTransformers.Tokens()
+        # )
+
+    def parseFile(self, fpath: TextIOBase | Path | str, ftype: str = ""):
         if isinstance(fpath, str):
             fpath = Path(fpath)
 
-        txt = fpath.read_text("latin-1")
-        return self.parse(txt)
+        if isinstance(fpath, Path):
+            txt = fpath.read_text("latin-1")
+            return self.parse(txt)
+        elif isinstance(fpath, TextIOBase):
+            txt = fpath.read()
+            return self.parse(txt)
 
     def parse(self, txt: str):
         raise NotImplementedError("Verilog parsing not yet supported!")
@@ -110,7 +146,7 @@ def count(tree):
 
 
 class VhdlParser:
-    def __init__(self, use_regex=True, debug=False):
+    def __init__(self, ambig=False, use_regex=True, debug=False):
         try:
             import regex
 
@@ -118,52 +154,59 @@ class VhdlParser:
         except ModuleNotFoundError:
             use_regex = False
 
+        self.ambig = ambig
+
         self.parser = Lark(
             open(Path(__file__).parent / "vhdl-2008.lark", encoding="latin-1"),
             start="design_file",
             regex=use_regex,
             debug=debug,
-            ambiguity="explicit",
+            ambiguity="explicit" if ambig else "resolve",
             lexer="dynamic",
         )
         self.csttransformer = ast_utils.create_transformer(
             VhdlCstTransformer, VhdlParseTreeTransformers.Tokens()
         )
 
-    def parseFile(self, fpath: Path | str, ftype: str = ""):
+    def parseFile(self, fpath: TextIOBase | Path | str, ftype: str = ""):
         if isinstance(fpath, str):
             fpath = Path(fpath)
 
-        txt = fpath.read_text("latin-1")
-        return self.parse(txt)
+        if isinstance(fpath, Path):
+            txt = fpath.read_text("latin-1")
+            return self.parse(txt)
+        elif isinstance(fpath, TextIOBase):
+            txt = fpath.read()
+            return self.parse(txt)
 
     def parse(self, txt: str):
         # parse code to tree
         parse_tree = self.parser.parse(txt)
 
         # remove and count ambiguities
-        if False:
-            from colorama import Fore
+        if self.ambig:
+            if False:
+                from colorama import Fore
 
-            parser2 = Lark(
-                open("hdltree/vhdl-2008.lark", encoding="latin-1"),
-                start="design_file",
-                regex=True,
-            )
-            parse_tree2 = parser2.parse(txt)
-            count(parse_tree)
-            parse_tree = VhdlParseTreeTransformers.MakeAmbigUnique().transform(parse_tree)
-            count(parse_tree)
-            parse_tree = VhdlParseTreeTransformers.CollapseAmbig().transform(parse_tree)
-            match = parse_tree == parse_tree2
-            print(
-                "disambiguated tree matches: "
-                + (Fore.GREEN if match else Fore.RED)
-                + str(match)
-                + Fore.RESET
-            )
-        else:
-            parse_tree = VhdlParseTreeTransformers.CollapseAmbig().transform(parse_tree)
+                parser2 = Lark(
+                    open("hdltree/vhdl-2008.lark", encoding="latin-1"),
+                    start="design_file",
+                    regex=True,
+                )
+                parse_tree2 = parser2.parse(txt)
+                count(parse_tree)
+                parse_tree = VhdlParseTreeTransformers.MakeAmbigUnique().transform(parse_tree)
+                count(parse_tree)
+                parse_tree = VhdlParseTreeTransformers.CollapseAmbig().transform(parse_tree)
+                match = parse_tree == parse_tree2
+                print(
+                    "disambiguated tree matches: "
+                    + (Fore.GREEN if match else Fore.RED)
+                    + str(match)
+                    + Fore.RESET
+                )
+            else:
+                parse_tree = VhdlParseTreeTransformers.CollapseAmbig().transform(parse_tree)
 
         # convert parse tree to custom format
         # try:
@@ -177,29 +220,80 @@ class VhdlParser:
         return cst
 
 
+def is_excluded(inpath: Path, excluded: List[Path]):
+    for ex in excluded:
+        if inpath.is_relative_to(ex):
+            return True
+    return False
+
+
 def main():
-    ve = VhdlParser()
-    code = '''
-package foo is
-  function afunc(q,w,e : std_ulogic; h,j,k : unsigned) return std_ulogic;
+    parser = ArgumentParser(description="Pure Python HDL parser")
+    parser.add_argument("-i", "--input", action="append", help="HDL source file or directory")
+    parser.add_argument(
+        "-p", "--print-tree", action="store_true", help="Print the parsed tree to stdout"
+    )
+    parser.add_argument(
+        "-a",
+        "--ambig",
+        action="store_true",
+        help="Instruct the parser to return an ambiguous tree and resolve them according to semantic rules",
+    )
+    parser.add_argument(
+        "-e",
+        "--exclude",
+        action="append",
+        default=[],
+        help="Files and directories to ignore",
+    )
+    args, unparsed = parser.parse_known_args()
+    # Allow file to be passed in without -i
+    if not args.input:
+        if len(unparsed) > 0:
+            args.input = unparsed
+        else:
+            args.input = ["."]
 
-  procedure aproc( r,t,y : in std_ulogic; u,i,o : out signed);
+    ve = HdlParser(ambig=args.ambig)
 
-  component acomp is
-    port (
-      a,b,c : in std_ulogic;    -- no default value
-      f,g,h : inout bit := '1'; -- bit ports
-      v : in std_logic_vector(lBound -1 downto 0) -- array range
-    ); -- port list comment
+    args.input = [Path(f) for f in args.input]
+    args.exclude = [Path(f) for f in args.exclude]
 
-  end component;
+    files = []
+    for inpath in args.input:
+        if inpath.is_file() and not is_excluded(inpath, args.exclude):
+            files = inpath
+        elif inpath.is_dir() and not is_excluded(inpath, args.exclude):
+            for ext in vhdl_fileext:
+                for infile in inpath.rglob("**/*." + ext):
+                    if not is_excluded(infile, args.exclude):
+                        files.append(infile)
 
-end package;
-  '''
+    for f in files:
+        print(f"analyzing {f}", end="", flush=True)
+        prev = time()
+        try:
+            cst = ve.parseFile(f)
+        except UnexpectedCharacters as e:
+            print()
+            print(f"error at line {e.line}, column {e.column}")
+            print("expected:")
+            print(e.allowed)
+            print("from rules:")
+            print(e.considered_rules)
+            print()
+        except Exception as e:
+            print(e)
 
-    cst = ve.parse(code)
-    richprint(cst.rich_tree())
+        lines = f.read_text().count("\n")
+        elapsed = time() - prev
+        print(
+            f"\ranalyzed {f} ({lines} lines) in {elapsed:.2f} seconds ({lines/elapsed:.2f} lines/sec)"
+        )
+
+        if args.print_tree:
+            richprint(cst.rich_tree())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
